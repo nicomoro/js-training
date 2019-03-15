5a9e8a866d74dada69afcf90d0961d878703b334
nico <nicomoro40@gmail.com>
2019-03-15 13:18:27 +0100

    solutions samesign et total

--- exercises/same-sign.js
+++ exercises/same-sign.js
@@ -8,6 +8,16 @@
  */
 
 // Your code :
+let a=3;
+let b=-6;
+function sameSign(a,b){
+    if ((a>0 && b>0)||(a<0 && b<0)||(a===0 && b===0)){
+        return true;
+    }
+    else {
+        return false;
+    }
+};
 
 //* Begin of tests
 const assert = require('assert')
--- exercises/total.js
+++ exercises/total.js
@@ -7,6 +7,14 @@
  */
 
 
+function total(arr){
+    let sum = 0;
+    for (let i=0; i<arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+};
+
 //* Begin of tests
 const assert = require('assert')
 
